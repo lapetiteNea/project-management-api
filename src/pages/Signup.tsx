a192ee4a1d84f76b340a76ce8fde8c75fbde93c8
lapetiteNea <neasharashidze@gmail.com>
2026-09-19 15:33:42 +0400

    feat: refactor Login with Formik, integrate Redux and disable buttons

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -67,6 +67,7 @@ export const Signup: React.FC = () => {
 
         <button
           type="submit"
+          disabled={formik.isSubmitting}
           className="mt-2 w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition-colors font-medium"
         >
           Sign Up
